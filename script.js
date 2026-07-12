b4812fee7b597edf2daecc758d4aa821aed55d34
israel-reis-pereira <israeldasilvabtos@gmail.com>
2026-07-12 20:18:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ moveButton.addEventListener("click", () => {
     moveButton.style.left = randomX + "px";
     moveButton.style.top = randomY + "px";
 });
+
 // Evento do botão "Sim"
 normalButton.addEventListener("click", () => {
-    window.open("https://youtu.be/dlE-DdOAI0E?si=UH7LQETJ38GLGKw_", "_blank");
     // alert("Finalmente se assumiu!");
+    window.open("https://youtu.be/dlE-DdOAI0E?si=UH7LQETJ38GLGKw_", "_blank");
 });
