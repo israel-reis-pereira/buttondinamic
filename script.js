f48b85d625f31c546289a5f07b95a88f6e16a02c
israel-reis-pereira <israeldasilvabtos@gmail.com>
2026-07-12 20:03:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const moveButton = document.getElementById("moveButton");
-const normalButton = document.getElementById("normalButton"); // Adicionada a declaração faltante
 
-// Alterado de 'click' para 'mouseover' para fugir antes do clique do mouse
-moveButton.addEventListener("mouseover", () => {
+moveButton.addEventListener("click", () => {
     const maxX = window.innerWidth - moveButton.clientWidth;
     const maxY = window.innerHeight - moveButton.clientHeight;
     
@@ -12,8 +10,8 @@ moveButton.addEventListener("mouseover", () => {
     moveButton.style.left = randomX + "px";
     moveButton.style.top = randomY + "px";
 });
-
 // Evento do botão "Sim"
 normalButton.addEventListener("click", () => {
+    // alert("Finalmente se assumiu!");
     window.open("https://youtu.be/dlE-DdOAI0E?si=UH7LQETJ38GLGKw_", "_blank");
 });
